efe3c5fcd585acb89bbf1a694ae17baa5fd7f072
Sophie <szaniszlozs7@gmail.com>
2020-02-06 15:50:29 +0100

    all done except restart

--- static/js/main.js
+++ static/js/main.js
@@ -9,30 +9,67 @@ function hideCards() {
     let cards = document.querySelectorAll('.card');
     for (let card of cards) {
         card.classList.add('hidden');
+        card.classList.remove('clicked');
     }
 }
 
 function showCards() {
     let cards = document.querySelectorAll('.card');
-    let clickedCardss = 0;
+    let clickedCards = 0;
     for (let card of cards) {
         card.addEventListener('click', function (e) {
-            clickedCardss++;
+            clickedCards++;
             card.classList.add('clicked');
             card.classList.remove('hidden');
-            clickedCards();
+            setTimeout(gotPair, 500);
         });
     }
 }
 
-function clickedCards() {
+function gotPair() {
     let clickedCards = document.querySelectorAll('.clicked');
     let cardName1 = clickedCards.item(0).dataset.name;
     let cardName2 = clickedCards.item(1).dataset.name;
     if (cardName1 === cardName2) {
-        alert('you got a pair');
+        for (let clickedCard of clickedCards) {
+            clickedCard.classList.remove('clicked');
+            clickedCard.classList.add('pairs');
+        }
+    } else {
+        hideCards();
     }
-    console.log(clickedCards);
+    ifWon();
+}
+
+function ifWon() {
+    let main = document.querySelector('.main');
+    let allPairs = document.querySelectorAll('.pairs');
+    let allPairList = Array.from(allPairs);
+    console.log(allPairList);
+    if (allPairList.length === 12) {
+        let win = document.createElement('div');
+        win.setAttribute('class', 'win');
+        win.innerHTML = `<p>You win!</p><div class="restart">Restart</div>
+        <img src="/static/cards/win.gif">`;
+        main.appendChild(win);
+    }
+    rePlay();
+}
+
+function rePlay() {
+    let restart = document.querySelector('.restart');
+    restart.addEventListener("mouseover", function () {
+        restart.classList.add('mouseon');
+    });
+    restart.addEventListener("mouseout", function () {
+        restart.classList.remove('mouseon');
+    });
+    restart.addEventListener("click", function () {
+        restart.classList.add('mouseclicked');
+        randomizedCards();
+        hideCards();
+        showCards();
+    })
 }
 
 randomizedCards();
