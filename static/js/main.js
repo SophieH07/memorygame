98972a4ee0ec997db919fd8aa97a0dea853e70cc
Sophie <szaniszlozs7@gmail.com>
2020-02-06 10:14:18 +0100

    Zsoltis vers

--- static/js/main.js
+++ static/js/main.js
@@ -14,28 +14,23 @@ function hideCards() {
 
 function showCards() {
     let cards = document.querySelectorAll('.card');
+    let clickedCardss = 0;
     for (let card of cards) {
         card.addEventListener('click', function (e) {
+            clickedCardss++;
             card.classList.add('clicked');
             card.classList.remove('hidden');
-            clickedCards(card);
+            clickedCards();
         });
     }
 }
 
-function clickedCards(card) {
+function clickedCards() {
     let clickedCards = document.querySelectorAll('.clicked');
-    let cardsList = Array.from(clickedCards);
-    for (let cardElement of cardsList) {
-        if (clickedCards.length === 2) {
-            let cardName1 = cardsList[0].dataset.name;
-            let cardName2 = cardsList[1].dataset.name;
-            if (cardName1 === cardName2) {
-                card.classList.add('clicked');
-                card.classList.remove('hidden');
-                alert('you got a pair');
-            }
-        }
+    let cardName1 = clickedCards.item(0).dataset.name;
+    let cardName2 = clickedCards.item(1).dataset.name;
+    if (cardName1 === cardName2) {
+        alert('you got a pair');
     }
     console.log(clickedCards);
 }
